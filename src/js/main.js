b4c3717c68d453e3e02f14e94e5081fa4bf1f7a4
Shabdan <shabdan.abjaparov,official@gmail.com>
2021-06-04 12:27:45 +0600

    init

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 5,
+        loop: true,
+        margin: 10,
+        center: true,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true
+    });
+});
